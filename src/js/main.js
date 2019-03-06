7f2fd7107c433182f3d6e287a1ff222f3ac3c57f
Andrey Sokolow <quick.sokolow94@gmail.com>
2019-03-06 19:04:34 +0300

    in progress

--- src/js/main.js
+++ src/js/main.js
@@ -1,105 +1,183 @@
-// $(document).ready(function() {
+$(document).ready(function() {
 
-	class Cookie {
-		constructor(name, value, options) {
-			this.name = name;
-			this.options = options;
-			this.value = value;
-		}
+	// class Cookie {
+	// 	constructor(name, value, options) {
+	// 		this.name = name;
+	// 		this.options = options;
+	// 		this.value = value;
+	// 	}
 
-		get() {
-			const matches = document.cookie.match(new RegExp(
-				"(?:^|; )" + this.name.replace(/([.$?*|{}()\[\]\\\/+^])/g, '\\$1') + "=([^;]*)"
-			));
+	// 	get() {
+	// 		const matches = document.cookie.match(new RegExp(
+	// 			"(?:^|; )" + this.name.replace(/([.$?*|{}()\[\]\\\/+^])/g, '\\$1') + "=([^;]*)"
+	// 		));
 
-			return matches ? decodeURIComponent(matches[1]) : undefined;
-		}
+	// 		return matches ? decodeURIComponent(matches[1]) : undefined;
+	// 	}
 
-		exist() {
-			return !!this.get();
-		}
+	// 	exist() {
+	// 		return !!this.get();
+	// 	}
 
-		set() {
-			let options = this.options,
-			expires = options.expires;
+	// 	set() {
+	// 		let options = this.options,
+	// 		expires = options.expires;
 
-			if (expires && typeof expires === "number") {
-				const d = new Date();
-				d.setTime(d.getTime() + expires * 1000);
-				expires = options.expires = d;
-			}
-			if (expires && expires.toUTCString) {
-				options.expires = expires.toUTCString();
-			}
+	// 		if (expires && typeof expires === "number") {
+	// 			const d = new Date();
+	// 			d.setTime(d.getTime() + expires * 1000);
+	// 			expires = options.expires = d;
+	// 		}
+	// 		if (expires && expires.toUTCString) {
+	// 			options.expires = expires.toUTCString();
+	// 		}
 
-			let value = encodeURIComponent(this.value);
+	// 		let value = encodeURIComponent(this.value);
 
-			let updatedCookie = this.name + "=" + value;
+	// 		let updatedCookie = `${this.name}=${value}`;
 
-			for (const propName in options) {
-				updatedCookie += "; " + propName;
-				const propValue = options[propName];
-				if (propValue !== true) {
-					updatedCookie += "=" + propValue;
-				}
-			}
+	// 		for (const propName in options) {
+	// 			updatedCookie += "; " + propName;
+	// 			const propValue = options[propName];
+	// 			if (propValue !== true) {
+	// 				updatedCookie += "=" + propValue;
+	// 			}
+	// 		}
 
-			document.cookie = updatedCookie;
-		}
+	// 		document.cookie = updatedCookie;
+	// 	}
 
-		destroy() {
-			this.options = {
-				expires: -1
-			};
-			this.value = "";
+	// 	destroy() {
+	// 		this.options = {
+	// 			expires: -1
+	// 		};
+	// 		this.value = "";
 
-			this.set();
-		}
-	}
-
-
-	let getCookie = new Cookie();
-
-	getCookie.name = 'name';
-	getCookie.value = 'vasya';
-	getCookie.options = 1000;
+	// 		this.set();
+	// 	}
+	// }
 
 
-	getCookie.set();
+	// let getCookie = new Cookie('name', 'vasya', 1000);
 
-	console.log(getCookie.get());
-	console.log(document.cookie);
-
-	let testObj = {
-		'1': 'value',
-		'2': 'value',
-		'asdasd': 'value'
-	}
-	console.log(testObj);
-	let testArr = [];
-
-	for (let prop in testObj) {
-		testArr.push(testObj[prop]);
-	}
+	// // getCookie.name = 'name';
+	// // getCookie.value = 'vasya';
+	// // getCookie.options = 1000;
 
 
-	console.log(testArr);
+	// getCookie.set();
 
-	let allInputs = [...document.querySelectorAll('input[type="text"]')];
+	// console.log(getCookie.get());
+	// console.log(document.cookie);
+
+	// let testObj = {
+	// 	'1': 'value',
+	// 	'2': 'value',
+	// 	'asdasd': 'value'
+	// }
+	// console.log(testObj);
+	// let testArr = [];
+
+	// for (let prop in testObj) {
+	// 	testArr.push(testObj[prop]);
+	// }
 
 
-	allInputs.map((input) => {	
-		input.addEventListener('change', function(){
+	// console.log(testArr);
 
-			allInputs.map((item) => {
-				if (item.value) {
-					console.log(item.value);
+	// let allInputs = [...document.querySelectorAll('input[type="text"]')];
+
+
+	// allInputs.map((input) => {	
+	// 	input.addEventListener('change', function(){
+
+	// 		allInputs.map((item) => {
+	// 			if (item.value) {
+	// 				console.log(item.value);
 
 					
 
-					return false;
-				}
-			})
-		})
-	})
-// });
+	// 				return false;
+	// 			}
+	// 		})
+	// 	})
+	// })
+
+
+	$('[data-date]').inputmask({"mask": "99.99.9999", showMaskOnHover: false,});
+	$('[data-phone]').inputmask({"mask": "+7 (999) 999-9999", showMaskOnHover: false,});
+	// $('[data-cost]').inputmask({"mask": "[9*{1,20}] руб", showMaskOnHover: false,});
+
+	$(document).on('input', '[data-cost]', function() {
+		let temp = $(this).val();
+
+		if (temp == 0) {
+			temp = 0;
+		}
+
+		temp = temp.replace(/\s+/g,'');
+		temp = parseInt(temp);
+		temp = temp.toLocaleString('ru');
+
+		$(this).val(`${temp}`);
+	});
+
+	$(document).on('change', '[data-cost]', function() {
+		let temp = $(this).val();
+
+		if (temp) {
+			$(this).val(`${temp} руб`)
+		}
+	});
+
+	$(document).on('focus', '.form-group__text input', function() {
+		$(this).closest('.form-group__text').addClass('active');
+	});
+
+	$(document).on('blur', '.form-group__text input', function() {
+		if ( $(this).val().length ) return false;
+		$(this).closest('.form-group__text').removeClass('active');
+	});
+
+	$(document).on('click', '.form-cross', function() {
+		$(this).siblings('input').val('');
+		$(this).closest('.form-group__text').removeClass('active');
+		$('.form-btn.form-btn__next').removeClass('active');
+	});
+
+	$(document).on('input', 'input[required]', function() {
+		let countInputs = $(document).find('.form-block.active').find('input').length;
+		let countFilledInputs = 0;
+
+		$(document).find('.form-block.active').find('input[required]').each(function(){
+			if ($(this).val()) {
+				return countFilledInputs++;
+			}
+		});
+
+		countInputs == countFilledInputs ? $(this).closest('.form-block').find('.form-btn.form-btn__next').addClass('active') : $(this).closest('.form-block').find('.form-btn.form-btn__next').removeClass('active')
+	});
+
+	$(document).on('click', '.form-btn.form-btn__next.active', function() {
+		let temp = $('.form-block.active').index('.form-block');
+		let allFormBlock = ($('.form-block').length - 1);
+
+		if (temp >= (allFormBlock)) {
+			return false;
+		} else {
+			$('.form-block').eq((temp + 1)).addClass('active');
+		}
+		$('.form-block').eq(temp).removeClass('active');
+	});
+
+	$(document).on('click', '.form-btn.form-btn__prev', function() {
+		let temp = $('.form-block.active').index('.form-block');
+
+		if (temp <= 0) {
+			return false;
+		} else {
+			$('.form-block').eq((temp - 1)).addClass('active');
+		}
+		$('.form-block').eq(temp).removeClass('active');
+	});
+});
